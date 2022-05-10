4c318e8e9146764918f447f4db3f8be3255c0b47
jostrr <jostrr@gmatc.matc.edu>
2022-05-09 22:24:47 -0500

    Edit Layout

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -8,9 +8,17 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
 
 import Header from "./header"
 import "./layout.css"
+
+const Content = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45rem;
+  padding-top: 0;
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,26 +33,19 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
+      <Header siteTitle={data.site.siteMetadata.title || `Title`} />
+      <Content>
         <main>{children}</main>
         <footer
           style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
+            marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()} &middot; Built with
+          {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
-      </div>
+      </Content>
     </>
   )
 }
