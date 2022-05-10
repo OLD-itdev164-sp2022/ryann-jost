b7f47bd56b99d05d51ae71c58a563c2625b0dc63
jostrr <jostrr@gmatc.matc.edu>
2022-05-10 16:16:52 -0500

    Completed Activity 5

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -4,7 +4,7 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Blog`,
+    title: `RJ's Blog`,
     description: `ITDEV-164 Gatsby Blog`,
     author: `Ryann Jost`,
     contact: {
