2890e0788b6ab7bf5f7967eee462ad2870ff46c3
jostrr <jostrr@gmatc.matc.edu>
2022-05-09 21:33:24 -0500

    Display image and content

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -30,7 +30,8 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-remark``gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
