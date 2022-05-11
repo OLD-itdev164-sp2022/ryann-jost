035cd6d1815855d86fac5c478e482571e264de93
jostrr <jostrr@gmatc.matc.edu>
2022-05-11 00:11:37 -0500

    Use icon button component

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from "styled-components"
 import { H1 } from "../Heading"
+import { IconButton } from "../Button"
 import { Section } from "../Section"
 
 const Outer = styled.header`
@@ -37,7 +38,9 @@ const Header = ({ siteTitle }) => (
             <StyledLink to="/">{siteTitle}</StyledLink>
           </H1>
         </Section>
-        <Section width={1 / 12}>Search</Section>
+        <Section width={1 / 12}>
+          <IconButton icon={<Search />} />
+        </Section>
       </Section>
     </Inner>
   </Outer>
